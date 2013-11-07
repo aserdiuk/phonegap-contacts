6b5e0fc94529bc230471c61a1398e35c8cd618ee
aserdiuk <test@default.test>
2013-11-07 21:47:30 +0200

    dummy commit

--- js/ru_RU.js
+++ js/ru_RU.js
@@ -4,21 +4,21 @@
  * */
 
 (function () {
-	var locUI = {
-			firstname: "Имя",
-			lastname : "Фамилия",
-			birthday : "DD.MM.YY",
-			email : "Email",
-			phone : "(XXX)XXXXXXX",
-			orderBtn: "Заказать",
-			topBackBtn : "Назад"
-	}
+    var locUI = {
+        firstname: "Имя",
+        lastname: "Фамилия",
+        birthday: "DD.MM.YY",
+        email: "Email",
+        phone: "(XXX)XXXXXXX",
+        orderBtn: "Заказать",
+        topBackBtn: "Назад"
+    };
 	document.getElementById("fname").setAttribute("placeholder", locUI.firstname);
 	document.getElementById("surname").setAttribute("placeholder", locUI.lastname);
 	document.getElementById("birthday").setAttribute("placeholder", locUI.birthday);
 	document.getElementById("email").setAttribute("placeholder", locUI.email);
 	document.getElementById("phone").setAttribute("placeholder", locUI.phone);
-	document.getElementById("topBack").setAttribute("back", locUI.topBackBtn);tets
+	document.getElementById("topBack").setAttribute("back", locUI.topBackBtn);
 })();	
 
 
